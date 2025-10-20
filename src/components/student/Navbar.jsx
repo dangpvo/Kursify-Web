f7b9732b16f3db8d039415dffc8c76d9b4e8c51e
Dennis Vo <dennisphuc@gmail.com>
2025-10-20 21:19:41 +0200

    feat: finish navbar for student view
    fix: adjust dummyDB to correct place

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const isCourseListPage = location.pathname.includes("/course-list");
+
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 bg-white">
+      <img
+        src={assets.logo}
+        alt="Logo"
+        className="w-28 lg:w-32 cursor-pointer"
+      />
+
+      <div className="hidden md:flex items-center gap-5 text-green-500">
+        <div className="flex items-center gap-5">
+          <button>
+            Become Educator | <Link to={"/my-enrollments"}>My Enrollments</Link>
+          </button>
+        </div>
+        <button className="bg-blue-600 text-white px-5 py-2 rounded-full">
+          Create Account
+        </button>
+      </div>
+
+      <div className="md:hidden"></div>
     </div>
   );
 };
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 export const AppContext = createContext();
 
-export const AppContextProvider = (children) => {
+export const AppContextProvider = ({ children }) => {
   const value = {};
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
