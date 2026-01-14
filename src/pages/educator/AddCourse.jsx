693e7ee45e70da2c7e93b9e0c022cb5dd25574d3
Dennis Vo <dennisphuc@gmail.com>
2026-01-14 11:06:18 +0100

    feat: finish some inputs in add course page of educator view (title, price, description with richtext by using Quilljs), discount and thumbnail

--- src/pages/educator/AddCourse.jsx
+++ src/pages/educator/AddCourse.jsx
@@ -1,9 +1,100 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import uniqid from "uniqid";
+import Quill from "quill";
+import { assets } from "../../assets/assets";
 
 const AddCourse = () => {
+  const quillRef = useRef(null);
+  const editorRef = useRef(null);
+
+  const [courseTitle, setCourseTitle] = useState("");
+  const [coursePrice, setCoursePrice] = useState(0);
+  const [discount, setDiscount] = useState(0);
+  const [image, setImage] = useState(null);
+  const [chapters, setChapters] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [currentChapterId, setCurrentChapterId] = useState(null);
+  const [lectureDetails, setLectureDetails] = useState({
+    lectureTitle: "",
+    lectureDuration: "",
+    lectureUrl: "",
+    isPreviewFree: false,
+  });
+
+  useEffect(() => {
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, { theme: "snow" }); //make sure editorRef can add RichText
+    }
+  }, []);
+
   return (
-    <div>
-      <h1>AddCourse</h1>
+    <div className="h-screen overflow-scroll flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0">
+      <form className="flex flex-col gap-4 max-w-md w-full text-gray-500">
+        <div className="flex flex-col gap-1">
+          <p>Course Title</p>
+          <input
+            type="text"
+            value={courseTitle}
+            onChange={(e) => setCourseTitle(e.target.value)}
+            placeholder="Course title"
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500"
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <p>Course Description</p>
+          <div ref={editorRef}></div>
+        </div>
+        <div className="flex items-center justify-between flex-wrap">
+          <div className="flex flex-col gap-1">
+            <p>Course Price</p>
+            <input
+              type="number"
+              placeholder="0"
+              value={coursePrice}
+              onChange={(e) => setCoursePrice(e.target.value)}
+              className="outline-none md:py-2.5 py-2 w-28 px-3 rounded border border-gray-500"
+              required
+            />
+          </div>
+
+          <div className="flex md:flex-row flex-col items-center gap-3">
+            <p>Course Thumbnail</p>
+            <label htmlFor="thumbnailImage" className="flex items-center gap-3">
+              <img
+                src={assets.file_upload_icon}
+                alt="upload file icon"
+                className="p-3 bg-blue-500 rounded"
+              />
+              <input
+                type="file"
+                id="thumbnailImage"
+                onChange={(e) => setImage(e.target.files[0])}
+                accept="image/*"
+                hidden
+              />
+              <img
+                src={image ? URL.createObjectURL(image) : ""}
+                alt=""
+                className="max-h-10"
+              />
+            </label>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <p>Discount (%)</p>
+          <input
+            type="number"
+            placeholder="0"
+            min={0}
+            max={100}
+            value={discount}
+            onChange={(e) => setDiscount(e.target.value)}
+            className="outline-none md:py-2.5 py-2 w-28 px-3 rounded border border-gray-500"
+          />
+        </div>
+      </form>
     </div>
   );
 };
